6f5480e6d4847bff7615e8f46eb14db4b32631b0
Marko Kljajic <marko_kole@yahoo.com>
2018-08-19 19:48:19 +0200

    moving block

--- main.tf
+++ main.tf
@@ -21,19 +21,19 @@ resource "aws_instance" "app" {
 resource "aws_key_pair" "deployer" {
   key_name   = "mykey"
   public_key = "${data.consul_keys.app.var.key_pair}"
+}
 
 data "aws_subnet" "selected" {
     id = "${data.consul_keys.app.var.subnet_id}"
 }
 
-  resource "aws_security_group" "subnet" {
-    vpc_id = "${data.aws_subnet.selected.vpc_id}"
+resource "aws_security_group" "subnet" {
+  vpc_id = "${data.aws_subnet.selected.vpc_id}"
 
-    ingress {
-      cidr_blocks = ["${data.aws_subnet.selected.cidr_block}"]
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-    }
+  ingress {
+    cidr_blocks = ["${data.aws_subnet.selected.cidr_block}"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
   }
 }
