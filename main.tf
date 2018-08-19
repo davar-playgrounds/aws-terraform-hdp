7e7d7a0668b1e82094bc5c72f8aad913193a762e
Marko Kljajic <marko_kole@yahoo.com>
2018-08-19 19:45:54 +0200

    added aws_subnet

--- main.tf
+++ main.tf
@@ -22,4 +22,18 @@ resource "aws_key_pair" "deployer" {
   key_name   = "mykey"
   public_key = "${data.consul_keys.app.var.key_pair}"
 
+data "aws_subnet" "selected" {
+    id = "${data.consul_keys.app.var.subnet_id}"
+}
+
+  resource "aws_security_group" "subnet" {
+    vpc_id = "${data.aws_subnet.selected.vpc_id}"
+
+    ingress {
+      cidr_blocks = ["${data.aws_subnet.selected.cidr_block}"]
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+    }
+  }
 }
