0a4d90c9243f2e7dd4eaf9dfa56b837517356beb
Marko Kljajic <marko_kole@yahoo.com>
2018-08-26 17:11:21 +0200

    param values changed

--- subnet/main.tf
+++ subnet/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_subnet" "terraform_subnet" {
-  vpc_id     = "vpc-06d960ad24df293c8" # "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/24"
+  vpc_id     = "vpc-d5259bb0" # "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/16"
 
   tags {
     Name = "Terraform Subnet"
