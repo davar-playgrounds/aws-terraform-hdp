3a11f93825ba0612da4ec11229f1a34e3e5c11bb
Marko Kljajic <marko_kole@yahoo.com>
2018-08-26 12:18:37 +0200

    added instance

---
+++ instance/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "${data.consul_keys.app.var.region}"
+}
+
+resource "aws_instance" "app" {
+  ami = "${data.consul_keys.app.var.ami}"
+  instance_type = "${data.consul_keys.app.var.instance_type}"
+  #availability_zone = "${data.consul_keys.app.var.availability_zone}"
+
+  tags {
+    Name = "${data.consul_keys.app.var.Name}"
+  }
+}
