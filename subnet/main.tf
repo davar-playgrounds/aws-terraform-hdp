c987f06b50e7ec93d7a2f78b2f6ad7c857cfe36f
markokole <marko_kole@yahoo.com>
2018-08-26 18:32:49 +0000

    subnet works

--- subnet/main.tf
+++ subnet/main.tf
@@ -7,8 +7,8 @@ module "vpc" {
 }
 
 resource "aws_subnet" "terraform_subnet" {
-  vpc_id     = "${module.vpc.vpc_id}" # "${module.vpc}"  #"vpc-d5259bb0" # "${aws_vpc.main.id}"
-  cidr_block = "172.31.32.0/20"
+  vpc_id     = "${module.vpc.vpc_id}"
+  cidr_block = "${module.vpc.cidr_block}"
 
   tags {
     Name = "Terraform Subnet"
