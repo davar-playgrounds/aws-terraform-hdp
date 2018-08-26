565f9742d9145311a469e0523102e147784dbbc6
markokole <marko_kole@yahoo.com>
2018-08-26 18:03:43 +0000

    render output values

--- subnet/main.tf
+++ subnet/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 }
 
 resource "aws_subnet" "terraform_subnet" {
-  vpc_id     = "vpc-d5259bb0" # "${aws_vpc.main.id}"
+  vpc_id     = "${module.vpc.vpc_id}" # "${module.vpc}"  #"vpc-d5259bb0" # "${aws_vpc.main.id}"
   cidr_block = "172.31.32.0/20"
 
   tags {
