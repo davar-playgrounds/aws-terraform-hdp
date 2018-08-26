45d29011604e6a9a9283a4d4ec62c408e2c1b3d0
markokole <marko_kole@yahoo.com>
2018-08-26 17:07:17 +0000

    subnet manual test

--- subnet/main.tf
+++ subnet/main.tf
@@ -2,9 +2,13 @@ provider "aws" {
   region = "${data.consul_keys.app.var.region}"
 }
 
+module "vpc" {
+  source = "../vpc"
+}
+
 resource "aws_subnet" "terraform_subnet" {
   vpc_id     = "vpc-d5259bb0" # "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/16"
+  cidr_block = "172.31.32.0/20"
 
   tags {
     Name = "Terraform Subnet"
