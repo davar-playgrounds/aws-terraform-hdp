4c6bd9354ef44b3d15a0144ca63bdf1faced134c
Marko Kljajic <marko_kole@yahoo.com>
2018-08-27 16:41:04 +0200

    typo

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "security-group" {
   vpc_id = "${module.vpc.vpc_id}"
 
   ingress {
-    cidr_blocks = ["${{module.vpc.cidr_block}"]
+    cidr_blocks = ["${module.vpc.cidr_block}"]
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
