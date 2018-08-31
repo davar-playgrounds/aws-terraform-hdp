38a4636c558716bb1825d5f45056cdec0572d184
Marko Kljajic <marko_kole@yahoo.com>
2018-08-31 21:33:05 +0200

    changes

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "${data.consul_keys.app.var.region}"
 }
 
-resource "aws_instance" "app" {
+resource "aws_instance" "test_instance" {
   ami = "${data.consul_keys.app.var.ami}"
   instance_type = "${data.consul_keys.app.var.instance_type}"
   #availability_zone = "${data.consul_keys.app.var.availability_zone}"
@@ -16,7 +16,7 @@ resource "consul_keys" "app" {
   datacenter = "${datacenter}"
 
   key {
-    path = "test/master/aws/test-instance/subnet_id"
-    value = "${aws_subnet.test_subnet.id}"
+    path = "test/master/aws/test-instance/instance_id"
+    value = "${aws_instance.test_instance.id}"
   }
 }
