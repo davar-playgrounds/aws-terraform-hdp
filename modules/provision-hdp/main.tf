8f1f57db5153a45a3a715eff30662b631eb28be0
Marko Kljajic <marko_kole@yahoo.com>
2018-08-31 22:22:46 +0200

    to-do added

--- modules/provision-hdp/main.tf
+++ modules/provision-hdp/main.tf
@@ -1,7 +1,8 @@
 resource "null_resource" "clone_hdp_repo" {
-
   # Clone HDP repository
   provisioner "local-exec" {
     command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook resources/clone-ansible-hortonworks.yml"
   }
 }
+
+# add install hdp depends_on
