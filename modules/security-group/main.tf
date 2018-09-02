b10c5c7434fef824b4f859dbff560df56f5f9334
Marko Kljajic <marko_kole@yahoo.com>
2018-09-02 08:10:29 +0200

    updated cidr and vpc variables

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -10,10 +10,10 @@ resource "aws_security_group" "security_group" {
 
   name = "${var.security_group_name}"
 
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "${data.consul_keys.app.var.vpc_id}"
 
   ingress {
-    cidr_blocks = ["${var.cidr_block}"]
+    cidr_blocks = ["${data.consul_keys.app.var.cidr_block}"]
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
