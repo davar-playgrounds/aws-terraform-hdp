3bba4b09f28e7d91a998b848fb47d4b293edd2c3
Marko Kljajic <marko_kole@yahoo.com>
2018-09-02 09:47:42 +0200

    change output

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -1,7 +1,7 @@
 output "vpc_id" {
-  value = "${aws_security_group.security_group.vpc_id}"
+  value = "${aws_security_group.terraform-security-group.vpc_id}"
 }
 
 output "security_group_id" {
-  value = "${aws_security_group.security_group.id}"
+  value = "${aws_security_group.terraform-security-group.id}"
 }
