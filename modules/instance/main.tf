1a772028d06442d81757c6f2b01309f18814e4c4
Marko Kljajic <marko_kole@yahoo.com>
2018-09-02 09:18:55 +0200

    test with aws_instance

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -2,17 +2,18 @@ provider "aws" {
   region = "${data.consul_keys.app.var.region}"
 }
 
-#resource "aws_instance" "test_instance" {
-resource "aws_launch_configuration" "test_instance" {
-  #ami = "${data.consul_keys.app.var.ami}"
-  image_id = "${data.consul_keys.app.var.ami}"
+resource "aws_instance" "test_instance" {
+#resource "aws_launch_configuration" "test_instance" {
+  ami = "${data.consul_keys.app.var.ami}"
+  #image_id = "${data.consul_keys.app.var.ami}"
   instance_type = "${data.consul_keys.app.var.instance_type}"
   security_groups = ["${data.consul_keys.app.var.security_group}"]
   #availability_zone = "${data.consul_keys.app.var.availability_zone}"
-  key_name = "${data.consul_keys.app.var.Name}"
-  #tags {
-  #  Name = "${data.consul_keys.app.var.Name}"
-  #}
+  #key_name = "${data.consul_keys.app.var.Name}"
+
+  tags {
+    Name = "${data.consul_keys.app.var.Name}"
+  }
 }
 
 resource "consul_keys" "app" {
