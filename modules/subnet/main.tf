79f2555d582c1e3711d7a2ed91c9ac5d97f1e5d2
Marko Kljajic <marko_kole@yahoo.com>
2018-09-02 18:34:24 +0200

    update

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_subnet" "test_subnet" {
   vpc_id     = "${data.consul_keys.app.var.vpc_id}"
   cidr_block = "${data.consul_keys.app.var.cidr_block}"
-  availability_zone = "$data.consul_keys.app.var.availability_zone"
+  availability_zone = "${data.consul_keys.app.var.availability_zone}"
 
   tags {
     Name = "Terraform Subnet"
