1cb87da16d85ad628c782201c20c84f34558cba1
Marko Kljajic <marko_kole@yahoo.com>
2018-09-06 10:05:18 +0200

    added route table

---
+++ modules/02-route-table/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "${data.consul_keys.app.var.region}"
+}
+
+resource "aws_route_table" "terraform_route_table" {
+  vpc_id = "${data.consul_keys.app.var.vpc_id}"
+
+  route {
+    cidr_block = "${data.consul_keys.app.var.cidr_block}"
+    gateway_id = "${data.consul_keys.app.var.igw_id}"
+  }
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "consul_keys" "app" {
+  datacenter = "${var.datacenter}"
+
+  key {
+    path = "test/master/aws/test-instance/terraform_route_table"
+    value = "${aws_route_table.terraform_route_table.id}"
+  }
+}
---
+++ modules/02-route-table/variables.tf
@@ -0,0 +1,35 @@
+variable "path_in_consul" {
+  default   = "test/master/aws/test-instance"
+}
+variable "consul_server" {
+  default   = "127.0.0.1"
+}
+variable "consul_port" {
+  default   = "8500"
+}
+variable "datacenter" {
+  default   = "dc1"
+}
+
+variable "name" {
+  default = "Terraform Route Table"
+}
+
+data "consul_keys" "app" {
+  key {
+    name    = "region"
+    path    = "${var.path_in_consul}/region"
+  }
+  key {
+    name    = "vpc_id"
+    path    = "${var.path_in_consul}/vpc_id"
+  }
+  key {
+    name    = "cidr_block"
+    path    = "${var.path_in_consul}/cidr_block"
+  }
+  key {
+    name    = "igw_id"
+    path    = "${var.path_in_consul}/igw_id"
+  }
+}
