fa0c6e419afcd1bce11318dbcab61bbb2dfff56e
Marko Kljajic <marko_kole@yahoo.com>
2018-09-06 12:43:52 +0200

    logic moved here

--- modules/02-route-table/main.tf
+++ modules/02-route-table/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "${data.consul_keys.app.var.region}"
 }
 
-resource "aws_route_table" "terraform_route_table" {
+/*resource "aws_route_table" "terraform_route_table" {
   vpc_id = "${data.consul_keys.app.var.vpc_id}"
-  
+
   route {
     cidr_block = "${var.cidr_block_all}"# "${data.consul_keys.app.var.cidr_block}"
     gateway_id = "${data.consul_keys.app.var.igw_id}"
@@ -14,7 +14,24 @@ resource "aws_route_table" "terraform_route_table" {
     Name = "${var.name}"
   }
 }
+*/
+resource "aws_route" "route" {
+  route_table_id            = "${data.consul_keys.app.var.main_route_table_id}"
+  gateway_id                = "${data.consul_keys.app.var.igw_id}"
+  destination_cidr_block    = "0.0.0.0/0"
+  #depends_on                = ["aws_route_table.testing"]
+}
 
+resource "consul_keys" "app" {
+  datacenter = "${var.datacenter}"
+
+  key {
+    path = "test/master/aws/test-instance/route_id"
+    value = "${aws_route.route.id}"
+  }
+}
+
+/*
 resource "consul_keys" "app" {
   datacenter = "${var.datacenter}"
 
@@ -22,4 +39,4 @@ resource "consul_keys" "app" {
     path = "test/master/aws/test-instance/terraform_route_table"
     value = "${aws_route_table.terraform_route_table.id}"
   }
-}
+}*/
