b32ae8e82518ce82edf37ab6b1036d42793a16b5
Marko Kljajic <marko_kole@yahoo.com>
2018-09-07 14:44:06 +0200

    rule for 8080 added

--- modules/04-security-group/main.tf
+++ modules/04-security-group/main.tf
@@ -4,12 +4,21 @@ provider "aws" {
 
 resource "aws_security_group_rule" "port_22_to_world" {
   type        = "ingress"
-  description = "Test rule"
+  description = "SSH to world"
   from_port   = "22"
   to_port     = "22"
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${data.consul_keys.app.var.default_security_group_id}"
+}
 
+resource "aws_security_group_rule" "port_8080_to_world" {
+  type        = "ingress"
+  description = "Ambari to world"
+  from_port   = "8080"
+  to_port     = "8080"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = "${data.consul_keys.app.var.default_security_group_id}"
 }
 
@@ -20,9 +29,8 @@ resource "consul_keys" "app" {
     path = "test/master/aws/test-instance/port_22_to_world"
     value = "${aws_security_group_rule.port_22_to_world.id}"
   }
-
-  /*key {
-    path = "test/master/aws/test-instance/security_group_name"
-    value = "${aws_security_group_rule.terraform-security-group.name}"
-  }*/
+  key {
+    path = "test/master/aws/test-instance/port_8080_to_world"
+    value = "${aws_security_group_rule.port_8080_to_world.id}"
+  }
 }
