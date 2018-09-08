3cba59472c61d63b855f6ad5d6e6e4b33516c154
Marko Kljajic <marko_kole@yahoo.com>
2018-09-08 16:05:22 +0200

    change back

--- modules/03-route-table/main.tf
+++ modules/03-route-table/main.tf
@@ -2,15 +2,10 @@ provider "aws" {
   region = "${data.consul_keys.app.var.region}"
 }
 
-module "gateway" {
-  source = "../02-gateway"
-}
-
 resource "aws_route" "route" {
   route_table_id            = "${data.consul_keys.app.var.main_route_table_id}"
   gateway_id                = "${data.consul_keys.app.var.igw_id}"
   destination_cidr_block    = "0.0.0.0/0"
-  #depends_on                = ["aws_route_table.testing"]
 }
 
 resource "consul_keys" "app" {
@@ -22,7 +17,7 @@ resource "consul_keys" "app" {
   }
 }
 
-/*
+
 resource "consul_keys" "app" {
   datacenter = "${var.datacenter}"
 
@@ -30,4 +25,4 @@ resource "consul_keys" "app" {
     path = "test/master/aws/test-instance/terraform_route_table"
     value = "${aws_route_table.terraform_route_table.id}"
   }
-}*/
+}
