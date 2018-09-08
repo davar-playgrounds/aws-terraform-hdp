bba78dc38f9e72495fe267e6031ecac74dbf1b6b
Marko Kljajic <marko_kole@yahoo.com>
2018-09-08 16:08:26 +0200

    update

--- modules/03-route-table/main.tf
+++ modules/03-route-table/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "${data.consul_keys.app.var.region}"
 }
 
-resource "aws_route" "terraform_route_table" {
+resource "aws_route" "route" {
   route_table_id            = "${data.consul_keys.app.var.main_route_table_id}"
   gateway_id                = "${data.consul_keys.app.var.igw_id}"
   destination_cidr_block    = "0.0.0.0/0"
@@ -14,15 +14,5 @@ resource "consul_keys" "app" {
   key {
     path = "test/master/aws/test-instance/route_id"
     value = "${aws_route.route.id}"
-  }
-}
-
-
-resource "consul_keys" "app" {
-  datacenter = "${var.datacenter}"
-
-  key {
-    path = "test/master/aws/test-instance/terraform_route_table"
-    value = "${aws_route.terraform_route_table.id}"
   }
 }
