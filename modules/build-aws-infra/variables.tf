ecdb61d895a2c5b4bba972ddb15e6e13ae48844e
Marko Kljajic <marko_kole@yahoo.com>
2018-09-08 10:06:21 +0200

    added file

---
+++ modules/build-aws-infra/variables.tf
@@ -0,0 +1,26 @@
+variable "path_in_consul" {
+  default   = "test/master/aws/test-instance"
+}
+variable "consul_server" {
+  default   = "127.0.0.1"
+}
+variable "consul_port" {
+  default   = "8500"
+}
+variable "datacenter" {
+  default   = "dc1"
+}
+variable "cidr_block" {
+  default = "10.0.0.0/24"
+}
+
+data "consul_keys" "app" {
+  key {
+    name    = "region"
+    path    = "${var.path_in_consul}/region"
+  }
+  key {
+    name    = "vpc_id"
+    path    = "${var.path_in_consul}/vpc_id"
+  }
+}
