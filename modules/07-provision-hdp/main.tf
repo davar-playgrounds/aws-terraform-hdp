9a30e9b05d09eb2a53375641deee5e19aeb3b274
Marko Kljajic <marko_kole@yahoo.com>
2018-09-08 16:36:28 +0200

    added install pip res

--- modules/07-provision-hdp/main.tf
+++ modules/07-provision-hdp/main.tf
@@ -26,7 +26,14 @@ resource "null_resource" "passwordless_ssh" {
   }
 }
 
+resource "null_resource" "install-pip" {
+  provisioner "local-exec" {
+    command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook ${local.workdir}/resources/install-pip.yml --inventory=${local.workdir}/output/ansible-hosts"
+  }
+}
+
 resource "null_resource" "update_jinja2" {
+  depends_on = ["null_resource.update_jinja2"]
   provisioner "local-exec" {
     command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook ${local.workdir}/resources/update-jinja2.yml --inventory=${local.workdir}/output/ansible-hosts"
   }
