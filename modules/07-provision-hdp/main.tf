27a45dda19d4c5af72a3a474ce14f4696c735247
Marko Kljajic <marko_kole@yahoo.com>
2018-09-20 13:33:16 +0200

    typo

--- modules/07-provision-hdp/main.tf
+++ modules/07-provision-hdp/main.tf
@@ -1,4 +1,4 @@
-local.resource "null_resource" "clone_hdp_repo" {
+"null_resource" "clone_hdp_repo" {
   # Clone HDP repository
   provisioner "local-exec" {
     command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook resources/clone-ansible-hortonworks.yml"
