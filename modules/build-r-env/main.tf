fda64beb56d1c646eb83b3c6cf0650c7bb1fb600
Marko Kljajic <marko_kole@yahoo.com>
2018-10-16 15:55:00 +0200

    ansible install R added

--- modules/build-r-env/main.tf
+++ modules/build-r-env/main.tf
@@ -22,3 +22,14 @@ resource "local_file" "ansible_hosts_render" {
   content  = "${data.template_file.ansible_hosts_tmpl.rendered}"
   filename = "${local.workdir}/ansible_hosts"
 }
+
+resource "null_resource" "install_r" {
+  depends_on = [
+    "module.r_server",
+    "local_file.ansible_hosts_render",
+  ]
+
+  provisioner "local-exec" {
+    command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook --inventory=${local.workdir}/output/ansible_hosts ${path.module}/resources/ansible/install_r.yml"
+  }
+}
