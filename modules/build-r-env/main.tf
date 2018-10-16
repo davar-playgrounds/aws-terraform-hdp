a1142763dc1d72d9b84bcf09ad5943175083cac8
Marko Kljajic <marko_kole@yahoo.com>
2018-10-16 12:49:08 +0200

    preparing r env

--- modules/build-r-env/main.tf
+++ modules/build-r-env/main.tf
@@ -1,40 +1,11 @@
 module r_server {
   source             = "../spectrum-scale"
+  cluster_type       = "r-server"
 }
 
-provider "aws" {
-  region = "${data.consul_keys.app.var.region}"
-}
-
-resource "aws_instance" "test_instance" {
-  count = "${data.consul_keys.app.var.no_instances}"
-  ami = "${data.consul_keys.app.var.ami}"
-  instance_type = "${data.consul_keys.app.var.instance_type}"
-  subnet_id = "${data.consul_keys.app.var.subnet_id}"
-  security_groups = ["${data.consul_keys.app.var.security_group}"]
-  availability_zone = "${data.consul_keys.app.var.availability_zone}"
-  key_name = "mykeypair"
-  associate_public_ip_address = "true"
-
-  tags {
-    Name = "${data.consul_keys.app.var.Name}"
+resource "null_resource" "ansible_galaxy" {
+  # Get ansible role from galaxy
+  provisioner "local-exec" {
+    command = "ansible-galaxy install debops.rstudio_server"
   }
-}
-
-resource "consul_keys" "app" {
-  datacenter = "${var.datacenter}"
-
-  key {
-    path = "test/master/aws/test-instance/single/instance_id"
-    value = "${aws_instance.test_instance.*.id[0]}"
-  }
-  key {
-    path = "test/master/aws/test-instance/single/public_ip"
-    value = "${aws_instance.test_instance.*.public_ip[0]}"
-  }
-  key {
-    path = "test/master/aws/test-instance/single/public_dns"
-    value = "${aws_instance.test_instance.*.public_dns[0]}"
-  }
-
 }
