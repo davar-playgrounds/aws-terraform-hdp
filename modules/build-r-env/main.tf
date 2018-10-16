0115323f4cec45c5516d9c3962f0a52a1e16f50b
Marko Kljajic <marko_kole@yahoo.com>
2018-10-16 15:28:25 +0200

    preparing for ansible install

--- modules/build-r-env/main.tf
+++ modules/build-r-env/main.tf
@@ -1,11 +1,24 @@
 module r_server {
-  source             = "../spectrum-scale"
+  source             = "../06-instance"
   cluster_type       = "r-server"
 }
 
-resource "null_resource" "ansible_galaxy" {
-  # Get ansible role from galaxy
-  provisioner "local-exec" {
-    command = "ansible-galaxy install debops.rstudio_server"
+locals {
+  public_ip = "${data.consul_keys.app.var.public_ip}"
+  public_dns = "${data.consul_keys.app.var.public_dns}"
+  workdir="${path.cwd}/output"
+}
+
+data "template_file" "ansible_hosts_tmpl" {
+  template = "${file("${path.module}/resources/templates/ansible_hosts.tmpl")}"
+
+  vars {
+    public_ip = "${local.public_ip}"
+    public_dns = "${local.public_dns}"
   }
+}
+
+resource "local_file" "ansible_hosts_render" {
+  content  = "${data.template_file.ansible_hosts_tmpl.rendered}"
+  filename = "${local.workdir}/ansible_hosts"
 }
--- modules/build-r-env/variables.tf
+++ modules/build-r-env/variables.tf
@@ -6,10 +6,6 @@ variable "path_in_consul_aws" {
   default   = "test/master/aws/test-instance"
 }
 
-variable "path_in_consul_hdp" {
-  default   = "test/master/aws/"
-}
-
 variable "consul_server" {
   default   = "127.0.0.1"
 }
@@ -24,35 +20,11 @@ variable "datacenter" {
 
 data "consul_keys" "app" {
   key {
-    name    = "region"
-    path    = "${var.path_in_consul_aws}/region"
+    name    = "public_ip"
+    path    = "${var.path_in_consul_aws}${var.cluster_type}/public_ip"
   }
   key {
-    name    = "ami"
-    path    = "${var.path_in_consul_aws}/ami_id"
-  }
-  key {
-    name    = "security_group"
-    path    = "${var.path_in_consul_aws}/default_security_group_id"
-  }
-  key {
-    name    = "availability_zone"
-    path    = "${var.path_in_consul_aws}/availability_zone"
-  }
-  key {
-    name    = "subnet_id"
-    path    = "${var.path_in_consul_aws}/subnet_id"
-  }
-  key {
-    name    = "no_instances"
-    path    = "${var.path_in_consul_hdp}${var.cluster_type}/no_instances"
-  }
-  key {
-    name    = "instance_type"
-    path    = "${var.path_in_consul_hdp}${var.cluster_type}/instance_type"
-  }
-  key {
-    name    = "Name"
-    path    = "${var.path_in_consul_aws}${var.cluster_type}/tags/Name"
+    name    = "public_dns"
+    path    = "${var.path_in_consul_aws}${var.cluster_type}/public_dns"
   }
 }
