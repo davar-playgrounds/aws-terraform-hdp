3a35fd8a7891f9f4955db5278948b46fc5c1fe16
Marko Kljajic <marko_kole@yahoo.com>
2018-10-17 17:46:31 +0200

    fix

--- modules/single/main.tf
+++ modules/single/main.tf
@@ -32,15 +32,15 @@ resource "consul_keys" "app" {
   datacenter = "${var.datacenter}"
   key {
     path = "test/master/aws/test-instance/single/${var.cluster_type}/instance_id"
-    value = "${aws_instance.test_instance.id[0]}"
+    value = "${aws_instance.test_instance.id}"
   }
   key {
     path = "test/master/aws/test-instance/single/${var.cluster_type}/public_ip"
-    value = "${aws_instance.test_instance.*.public_ip[0]}"
+    value = "${aws_instance.test_instance.*.public_ip}"
   }
   key {
     path = "test/master/aws/test-instance/single/${var.cluster_type}/public_dns"
-    value = "${aws_instance.test_instance.*.public_dns[0]}"
+    value = "${aws_instance.test_instance.*.public_dns}"
   }
 
 }
