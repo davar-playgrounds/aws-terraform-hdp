98a07dc549d36e9d3d067c1419bd2f5fa30cb62a
Marko Kljajic <marko_kole@yahoo.com>
2018-10-18 12:52:11 +0200

    workaround

--- modules/build-r-env/main.tf
+++ modules/build-r-env/main.tf
@@ -1,27 +1,22 @@
-module "r_server" {
-  source             = "../single"
-  cluster_type       = "r-server"
-}
-
-resource "null_resource" "delay" {
-  depends_on = [
-    "module.r_server"
-  ]
-  provisioner "local-exec" {
-    command = "sleep 10"
-  }
-}
+#module "r_server" {
+#  source             = "../single"
+#  cluster_type       = "r-server"
+#}
 
 locals {
   #public_ip = "${data.consul_keys.app.var.public_ip}"
   #public_dns = "${data.consul_keys.app.var.public_dns}"
+  singledir = "${path.cwd}/../single"
   workdir="${path.cwd}/output"
 }
 
+resource "null_resource" "create_server" {
+  provisioner "local-exec" {
+    command = "cd ${singledir}; terraform apply -auto-approve -var cluster_type=r-server"
+  }
+}
+
 data "template_file" "ansible_hosts_tmpl" {
-  depends_on = [
-    "null_resource.delay"
-  ]
   template = "${file("${path.module}/resources/templates/ansible-hosts.tmpl")}"
 
   vars {
@@ -31,6 +26,9 @@ data "template_file" "ansible_hosts_tmpl" {
 }
 
 resource "local_file" "ansible_hosts_render" {
+  depends_on = [
+    "null_resource.create_server"
+  ]
   content  = "${data.template_file.ansible_hosts_tmpl.rendered}"
   filename = "${local.workdir}/ansible_hosts"
 }
