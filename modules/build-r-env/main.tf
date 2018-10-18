c35742f225ca25559ce4ef8838bc4c8979439743
Marko Kljajic <marko_kole@yahoo.com>
2018-10-18 14:34:40 +0200

    code changed to assume r-server IP is in consul

--- modules/build-r-env/main.tf
+++ modules/build-r-env/main.tf
@@ -1,34 +1,20 @@
-#module "r_server" {
-#  source             = "../single"
-#  cluster_type       = "r-server"
-#}
-
 locals {
-  #public_ip = "${data.consul_keys.app.var.public_ip}"
-  #public_dns = "${data.consul_keys.app.var.public_dns}"
+  public_ip = "${data.consul_keys.app.var.public_ip}"
+  public_dns = "${data.consul_keys.app.var.public_dns}"
   singledir = "${path.cwd}/../single"
   workdir="${path.cwd}/output"
-}
-
-resource "null_resource" "create_server" {
-  provisioner "local-exec" {
-    command = "cd "${singledir}"; terraform apply -auto-approve -var cluster_type=r-server"
-  }
 }
 
 data "template_file" "ansible_hosts_tmpl" {
   template = "${file("${path.module}/resources/templates/ansible-hosts.tmpl")}"
 
   vars {
-    public_ip = "${data.consul_keys.app.var.public_ip}" #"${local.public_ip}"
-    public_dns = "${data.consul_keys.app.var.public_dns}" #"${local.public_dns}"
+    public_ip = "${local.public_ip}"
+    public_dns = "${local.public_dns}"
   }
 }
 
 resource "local_file" "ansible_hosts_render" {
-  depends_on = [
-    "null_resource.create_server"
-  ]
   content  = "${data.template_file.ansible_hosts_tmpl.rendered}"
   filename = "${local.workdir}/ansible_hosts"
 }
