6a0d6a5d8b2197e77a07b5a244d7b011bba50487
Marko Kljajic <marko_kole@yahoo.com>
2018-12-28 17:36:24 +0100

    Update main.tf

--- modules/07-provision-hdp/main.tf
+++ modules/07-provision-hdp/main.tf
@@ -6,9 +6,9 @@ module "provision_hdp" {
 # all variables used in the script are in locals block
 locals {
 
-  type = "${data.consul_keys.hdp.var.type}" # single, classic or skatt
+  type = "${data.consul_keys.hdp.var.type}" # single or cluster
   no_instances = "${data.consul_keys.hdp.var.no_instances}" # number of servers in cluster
-  no_namenodes = "${local.type == "classic" ? data.consul_keys.hdp.var.no_namenodes : 0}" # number of namenodes in cluster
+  no_namenodes = "${local.type == "cluster" ? data.consul_keys.hdp.var.no_namenodes : 0}" # number of namenodes in cluster
 
   workdir="${path.cwd}/output/hdp-server/${local.clustername}"
 
@@ -27,19 +27,13 @@ locals {
   # indices to create the dynamic code in case single node cluster is also used
   # if a single node cluster -> indices are 0, else second server is namenode 1,
   # third server is namenode 2 and all the others are datanodes
-  #namenode_1_idx = "${local.no_instances == 1 ? 0 : 1}"
-  #namenode_2_idx = "${local.no_instances == 1 ? 0 : 2}"
   namenode_idx = "${local.no_instances == 1 ? 0 : 1}"
   datanode_idx = "${local.no_instances == 1 ? 0 : 1 + local.no_namenodes}"
 
 
-  # next no_namenodes servers are dedicated namenodes
+  # next 2 servers are dedicated namenodes
   # namenode_dns holds value of dns for the HDP cluster (Ambari server excluded)
   # these are only used when multinode cluster, otherwise they point to first server
-  #namenode_1_dns = "${local.public_dns[local.namenode_1_idx]}"
-  #namenode_1_ips = "${local.public_ips[local.namenode_1_idx]}"
-  #namenode_2_dns = "${local.public_dns[local.namenode_2_idx]}"
-  #namenode_2_ips = "${local.public_ips[local.namenode_2_idx]}"
   namenodes_dns = "${slice(local.public_dns, local.namenode_idx, local.namenode_idx + local.no_namenodes)}"
   namenodes_ips = "${slice(local.public_ips, local.namenode_idx, local.namenode_idx + local.no_namenodes)}"
 
@@ -83,109 +77,61 @@ locals {
 data "template_file" "ansible_inventory_single" {
   count = "${local.type == "single" ? 1 : 0}"
   template = "${file("${path.module}/resources/templates/ansible_inventory_single.yml.tmpl")}"
-
   vars {
     ansible_hdp_master_name = "${local.public_dns[0]}"
     ansible_hdp_master_hosts = "${local.public_ips[0]}"
   }
 }
-/*
-# create the yaml file based on template and the input values
-resource "local_file" "ansible_inventory_single" {
-  count = "${local.type == "single" ? 1 : 0}" # execute if single
-  content  = "${data.template_file.ansible_inventory_single.rendered}"
-  filename = "${local.workdir}/ansible-hosts"
-}*/
-###
 
 ### CLUSTER ###
-# the cluster with have one server (first one) dedicated for cluster management -
+# the cluster has one server (first one) dedicated for cluster management -
 # Ambari services are running on it, Postgres metastore and some Hadoop related
-# services that cannot be made into HA
+# services that cannot be made into HA are running on it
 # Cluster design is inspired by the examples from the ansible-hortonworks repository
 
 # generate a datanode file - one datanode per line
 # this file is used later in the process to render the ansible-hosts file
-data "template_file" "generate_datanode_hostname_classic" {
-  count = "${local.type == "classic" ? local.no_instances - local.no_namenodes : 0}" # workaround
-  #count = "${length(local.datanodes_dns)}" # do i need this to be repeated n-times?
+data "template_file" "generate_datanode_hostname_cluster" {
+  count = "${local.type == "cluster" ? local.no_instances - local.no_namenodes : 0}" # workaround
   template = "${file("${path.module}/resources/templates/datanode_hostname.tmpl")}"
   vars {
     datanode-text = "${element(local.datanodes_ips, count.index)} ansible_host=${element(local.datanodes_dns, count.index)} ansible_user=centos ansible_ssh_private_key_file=\"~/.ssh/id_rsa\""
   }
 }
 
-data "template_file" "generate_namenode_hostname_classic" {
-  count = "${local.type == "classic" ? local.no_namenodes : 0}"
+data "template_file" "generate_namenode_hostname_cluster" {
+  count = "${local.type == "cluster" ? local.no_namenodes : 0}"
   template = "${file("${path.module}/resources/templates/namenode_hostname.tmpl")}"
-
   vars {
     host-group-name = "[hdp-master-0${count.index + 1}]"
     namenode-text = "${element(local.namenodes_ips, count.index)} ansible_host=${element(local.namenodes_dns, count.index)} ansible_user=centos ansible_ssh_private_key_file=\"~/.ssh/id_rsa\""
   }
 }
 
-# ansible-hosts file for classic Hadoop architecture is rendered here
+# ansible-hosts file for cluster Hadoop architecture is rendered here
 # populate the cluster template file
-data "template_file" "ansible_inventory_classic" {
-  count = "${local.type == "classic" ? 1 : 0}"
+data "template_file" "ansible_inventory_cluster" {
+  count = "${local.type == "cluster" ? 1 : 0}"
   template = "${file("${path.module}/resources/templates/ansible_inventory_cluster.yml.tmpl")}"
   vars {
     ambari-services-title = "[ambari-services]"
     ambari-ansible-text = "${local.ambari_ips} ambari_host=${local.ambari_dns} ansible_user=centos ansible_ssh_private_key_file=\"~/.ssh/id_rsa\""
-    #hdp-master-01-title = "[hdp-master-01]"
-    #namenode1-ansible-text = "${local.namenode_1_ips} ambari_host=${local.namenode_1_dns} ansible_user=centos ansible_ssh_private_key_file=\"~/.ssh/id_rsa\""
-    #hdp-master-02-title = "[hdp-master-02]"
-    #namenode2-ansible-text = "${local.namenode_2_ips} ambari_host=${local.namenode_2_dns} ansible_user=centos ansible_ssh_private_key_file=\"~/.ssh/id_rsa\""
-    namenode-ansible-text = "${join("",data.template_file.generate_namenode_hostname_classic.*.rendered)}"
+    namenode-ansible-text = "${join("",data.template_file.generate_namenode_hostname_cluster.*.rendered)}"
     hdp-worker-title = "[hdp-worker]"
-    datanode-ansible-text = "${join("",data.template_file.generate_datanode_hostname_classic.*.rendered)}"
+    datanode-ansible-text = "${join("",data.template_file.generate_datanode_hostname_cluster.*.rendered)}"
   }
 }
-/*
-# generate a temporary file with all datanodes - one datanode per line
-data "template_file" "generate_datanode_hostname_skatt" {
-  count = "${local.type == "skatt" ? 1 : 0}"
-  #count = "${length(local.datanodes_dns)}" # do i need this to be repeated n-times?
-  template = "${file("${path.module}/resources/templates/datanode_hostname.tmpl")}"
-
-  vars {
-    datanode-text = "${element(local.datanodes_ips, count.index)} ansible_host=${element(local.datanodes_dns, count.index)} ansible_user=centos ansible_ssh_private_key_file=\"~/.ssh/id_rsa\""
-  }
-}
-*/
-/*
-###  ansible-hosts file for skatt Hadoop architecture is rendered here
-data "template_file" "ansible_inventory_skatt" {
-  count = "${local.type == "classic" ? 1 : 0}"
-  template = "${file("${path.module}/resources/templates/ansible_hdp_cluster.yml.tmpl")}"
-  vars {
-    ambari-services-title = "[ambari-services]"
-    ambari-ansible-text = "${local.ambari_ips} ambari_host=${local.ambari_dns} ansible_user=centos ansible_ssh_private_key_file=\"~/.ssh/id_rsa\""
-    hdp-master-01-title = ""
-    namenode1-ansible-text = ""
-    hdp-master-02-title = ""
-    namenode2-ansible-text = ""
-    hdp-worker-title = "[hdp-master]"
-    datanode-ansible-text = "${join("",data.template_file.generate_datanode_hostname.*.rendered)}"
-  }
-}
-*/
 
 # create the yaml file based on template and the input values
 resource "local_file" "ansible_inventory_single_render" {
-  #count = "${local.single}"
   count = "${local.type == "single" ? 1 : 0}"
-
   content  = "${data.template_file.ansible_inventory_single.rendered}"
   filename = "${local.workdir}/ansible-hosts"
 }
 
-resource "local_file" "ansible_inventory_classic_render" {
-  #count = "${1 - local.single}"
-  count = "${local.type == "classic" ? 1 : 0}"
-
-  content  = "${data.template_file.ansible_inventory_classic.rendered}"
+resource "local_file" "ansible_inventory_cluster_render" {
+  count = "${local.type == "cluster" ? 1 : 0}"
+  content  = "${data.template_file.ansible_inventory_cluster.rendered}"
   filename = "${local.workdir}/ansible-hosts"
 }
 
@@ -195,7 +141,7 @@ resource "local_file" "ansible_inventory_classic_render" {
 # second file that is rendered is the cluster configuration file
 # there is one block in template - variable blueprint-dynamic - which depends on whether
 # it is a single node or a multi node cluster. That block defines which services and
-# clients should be installed to which host groups
+# clients should be installed on which host groups
 # The idea is to make this dynamic to avoid updating two templates
 # To make this work, interim files are used for that block
 
@@ -210,8 +156,9 @@ data "template_file" "generate_blueprint_dynamic_single" {
 }
 
 # generate the block with n_namenodes and their clients and services
+# in case of HA there will be two host_group block - 01 and 02
 data "template_file" "generate_blueprint_master_block" {
-  count = "${local.type == "classic" ? local.no_namenodes : 0}"
+  count = "${local.type == "cluster" ? local.no_namenodes : 0}"
   template = "${file("${path.module}/resources/templates/blueprint_dynamic_host_group_master.tmpl")}"
 
   vars {
@@ -224,7 +171,7 @@ data "template_file" "generate_blueprint_master_block" {
 
 # generate the cluster blueprint_dynamic block of the hdp-config file
 data "template_file" "generate_blueprint_dynamic_cluster" {
-  count = "${local.type == "classic" ? 1 : 0}"
+  count = "${local.type == "cluster" ? 1 : 0}"
   template = "${file("${path.module}/resources/templates/blueprint_dynamic_cluster.tmpl")}"
   vars {
     ambari_services = "${local.ambari_services}"
@@ -233,19 +180,6 @@ data "template_file" "generate_blueprint_dynamic_cluster" {
     slave_services = "${local.slave_services}"
   }
 }
-
-/*
-### generate the skatt blueprint_dynamic block of the hdp-config file
-data "template_file" "generate_blueprint_dynamic_skatt" {
-  count = "${local.type == "skatt" ? 1 : 0}"
-  template = "${file("${path.module}/resources/templates/blueprint_dynamic_skatt.tmpl")}"
-  vars {
-    ambari_services = "${local.ambari_services}"
-    master_clients = "${local.master_clients}"
-    master_services = "${local.master_services}"
-  }
-}
-*/
 
 ### prepare hdp config file
 data "template_file" "hdp_config" {
@@ -257,8 +191,7 @@ data "template_file" "hdp_config" {
     hdp_version = "${local.hdp_version}"
     hdp_build_number = "${local.hdp_build_number}"
     database = "${local.database}"
-    # use data.template_file depending on whether single or cluster
-    #blueprint_dynamic = "${join("",data.template_file.generate_blueprint_dynamic_local.type.*.rendered)}"
+    # the blueprint_dynamic block is built based on type is single or cluster
     blueprint_dynamic = "${local.type == "single" ? join("", data.template_file.generate_blueprint_dynamic_single.*.rendered) : join("", data.template_file.generate_blueprint_dynamic_cluster.*.rendered)}"
   }
 }
@@ -300,7 +233,7 @@ resource "null_resource" "prepare_nodes" {
   depends_on = [
     "null_resource.install_python_packages",
     "local_file.ansible_inventory_single_render",
-    "local_file.ansible_inventory_classic_render",
+    "local_file.ansible_inventory_cluster_render",
     "local_file.hdp_config_rendered"
   ]
   provisioner "local-exec" {
