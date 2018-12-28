06f92c50c0a654648214a4d439ae54bdba3638fb
Marko Kljajic <marko_kole@yahoo.com>
2018-12-28 09:03:45 +0100

    Update variables.tf

--- modules/07-provision-hdp/variables.tf
+++ modules/07-provision-hdp/variables.tf
@@ -21,7 +21,7 @@ data "consul_keys" "hdp" {
 
   key {
     name = "no_namenodes"
-    #path = "${var.path_in_consul_hdp}${var.cluster_type}/no_namenodes"
+    path = "${var.path_in_consul_hdp}${var.cluster_type}/no_namenodes_NOT_IN_USE"
     default = "2"
   }
 
